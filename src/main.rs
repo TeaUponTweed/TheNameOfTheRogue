bf7f63a0a6581b9758df2756c408804a0c126749
Michael Mason <TeaUponTweed@gmail.com>
2016-09-10 22:53:21 -0600

    give up and use curses for now

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,56 @@
-extern crate graphics;
-extern crate glutin_window;
-extern crate opengl_graphics;
-extern crate piston;
-extern crate vec_map;
-extern crate clock_ticks;
+extern crate specs;
+extern crate ncurses;
 
-use piston::window::WindowSettings;
-use piston::event_loop::*;
-use piston::input::*;
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL};
-use graphics::rectangle::square;
-#[macro_use] mod ecs;
-use ecs::*;
-use vec_map::VecMap;
+use std::{thread, time};
 
-#[derive(Copy, Clone, Debug)]
-pub struct Pos(f64, f64);
-#[derive(Copy, Clone, Debug)]
-pub struct Vel(f64, f64);
-
-world!{
-    name: Game,
-    components: {
-        pos => Pos,
-        vel => Vel
-    },
-    processors: {
-        movement => Movement for [pos, vel]
-    }
-}
-#[derive(Copy, Default, Clone, Debug)]
-pub struct Movement;
-impl Movement {
-    fn run(&mut self, delta: f64, entities: &[Entity], pos: &mut Components<Pos>, vel: &Components<Vel>) {
-        for &e in entities.iter() {
-            let Vel(vx, vy) = vel[e];
-            let &mut Pos(ref mut x, ref mut y) = &mut pos[e];
-            *x += vx * delta;
-            *y += vy * delta;
-        }
-    }
+struct Pos(f32, f32);
+impl specs::Component for Pos {
+    type Storage = specs::VecStorage<Pos>;
 }
 
+struct Vel(f32, f32);
+impl specs::Component for Vel {
+    type Storage = specs::VecStorage<Vel>;
+}
+
+struct Disp(char);
+impl specs::Component for Disp {
+    type Storage = specs::VecStorage<Disp>;
+}
 
 pub fn main() {
-    let mut world:Game = World::new();
-    let entity = world.create();
-    let entity1 = world.create();
-    // println!("{:?}", entity1);
-    world.pos.insert(entity, Pos(1.0, -2.0));
-    world.pos.insert(entity1, Pos(10.0, 10.0));
-    world.vel.insert(entity, Vel(3.0, 2.0));
-    world.vel.insert(entity1, Vel(30.0, 20.0));
-    for _ in 0..10 {
-        world.update(1.0);
-        for (id, &el) in world.pos.iter() {
-            // println!("{:?}", id);
-            println!("{:?}\n", el);
-        }
+    ncurses::initscr();
+
+    let mut planner : specs::Planner<()> = {
+        let mut w = specs::World::new();
+        // All components types should be registered before working with them
+        w.register::<Pos>();
+        w.register::<Vel>();
+        w.register::<Disp>();
+        // create_now() of World provides with an EntityBuilder to add components to an Entity
+        w.create_now().with(Pos(1.0, 6.0)).with(Vel(1.0, 2.0)).with(Disp('a')).build();
+        w.create_now().with(Pos(2.0, 5.0)).with(Vel(3.0, 4.0)).with(Disp('b')).build();
+        w.create_now().with(Pos(3.0, 4.0)).with(Disp('c')).build();
+        // Planner is used to run systems on the specified world with a specified number of threads
+        specs::Planner::new(w, 4)
+    };
+    loop {
+        ncurses::erase();
+        planner.run1w1r(|p: &mut Pos, v: &Vel| {
+            p.0 += v.0;
+            p.1 += v.1;
+            if p.0 > 100.0 {
+                p.0 = 0.0;
+            }
+            if p.1 > 100.0 {
+                p.1 = 0.0;
+            }
+        });
+        planner.run0w2r(|p: &Pos, c: &Disp| {
+            ncurses::mvaddch(p.1.floor() as i32, p.0.floor() as i32, c.0 as u64);
+        });
+        planner.wait();
+        ncurses::refresh();
+        thread::sleep(time::Duration::from_millis(200));
     }
 }
